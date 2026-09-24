f7b2e6b2732b905cb2fc91f189e9bdbb800e80ce
alirezasefidpour747 <alireza.sefidpour@gmail.com>
2026-09-24 12:01:26 +0330

    Wire Didar inquiry module and authentication middleware

--- templates/basic/packages/api/src/modules/didar-core/service.ts
+++ templates/basic/packages/api/src/modules/didar-core/service.ts
@@ -4,12 +4,16 @@ import DidarCatalogSource from "./models/catalog-source"
 import DidarMembership from "./models/membership"
 import DidarOrganization from "./models/organization"
 import DidarPiece from "./models/piece"
+import DidarInquiry from "./models/inquiry"
+import DidarStaffGrant from "./models/staff-grant"
 
 class DidarCoreService extends MedusaService({
   DidarOrganization,
   DidarMembership,
   DidarCatalogSource,
   DidarPiece,
+  DidarInquiry,
+  DidarStaffGrant,
 }) {}
 
 export default DidarCoreService
