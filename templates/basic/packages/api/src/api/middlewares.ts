7b503e34bf5f664ded3354201d16eea1770e1f3f
alirezasefidpour747 <alireza.sefidpour@gmail.com>
2026-09-24 12:01:27 +0330

    Wire Didar inquiry module and authentication middleware

--- templates/basic/packages/api/src/api/middlewares.ts
+++ templates/basic/packages/api/src/api/middlewares.ts
@@ -1,5 +1,14 @@
-import { defineMiddlewares } from "@medusajs/medusa";
+import { authenticate, defineMiddlewares } from "@medusajs/framework/http"
 
 export default defineMiddlewares({
-    routes: [],
-});
+  routes: [
+    {
+      matcher: "/store/didar/inquiries*",
+      middlewares: [authenticate("customer", ["session", "bearer"])],
+    },
+    {
+      matcher: "/admin/didar/inquiries*",
+      middlewares: [authenticate("user", ["session", "bearer"])],
+    },
+  ],
+})
